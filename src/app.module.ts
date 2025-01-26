8915707bf193d747dd3bb5bd1429fbba4d5101d1
Dagson Gabriel <dagson.g.01@gmail.com>
2025-01-26 00:03:00 -0300

    feat: cria conexão com banco de dados pelo TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'desafio_hubbi',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
