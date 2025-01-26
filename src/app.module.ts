b878cd4b7d6846a6d0f82939ec1f6be0d950eb95
Dagson Gabriel <dagson.g.01@gmail.com>
2025-01-26 00:30:47 -0300

    feat: configura variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import  * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'desafio_hubbi',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'admin',
+      database: process.env.DB_NAME || 'desafio_hubbi',
       entities: [],
       synchronize: true,
     }),
