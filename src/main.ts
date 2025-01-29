acc8c1bd9c57f58d35ea386f68a6bbc8caa1a8df
Dagson Gabriel <dagson.g.01@gmail.com>
2025-01-29 16:19:07 -0300

    feat: habilita CORS para permitir requisições

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3005);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET, POST, PATCH, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
